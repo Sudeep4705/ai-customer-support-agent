9c816184fad943477dc6597a49f557a8c01d05b4
Sudeep <sudeepsudee828@gmail.com>
2026-09-21 15:24:09 +0530

    feat: separate intent and customer TF-IDF models

--- backend/src/intent-tfidf.js
+++ backend/src/intent-tfidf.js
@@ -1,43 +1,32 @@
-import fs from "fs";
-import natural from "natural";
-import csv from "csv-parser";
-const tfidf = new natural.TfIdf();
-const intentsData = JSON.parse(
-  fs.readFileSync("../data/processed/final-intents.json", "utf8")
-);
-const intentTfidf = [];
-const descriptions = intentsData.intents.map(
-  (intent) => intent.description
-);
-console.log(descriptions);
-descriptions.forEach((description,index) => {
-  tfidf.addDocument(description);
-    const terms = tfidf.listTerms(index);
-  intentTfidf.push(terms);
-});
+  import fs from "fs";
+  import natural from "natural";
+  import csv from "csv-parser";
+const intentTfidf = new natural.TfIdf();
+const customerTfidf = new natural.TfIdf();
 
-const amazonhelpData = [];
-const customerTfidfData = [];
-fs.createReadStream("../data/processed/amazonhelp.csv")
-  .pipe(csv())
-  .on("data", (row) => {
-    amazonhelpData.push(row);
-  })
-  .on("end", () => {
-    console.log("Total rows:", amazonhelpData.length);
-    amazonhelpData.forEach((amazonhelp,index) => {
-  tfidf.addDocument(amazonhelp.customer_message);
-    amazonhelpData.forEach((amazonhelp, index) => {
-      const documentIndex = descriptions.length + index;
-      const terms = tfidf.listTerms(documentIndex);
-      customerTfidfData.push(terms);
-    });
-});
-    console.log("this is the intents number",intentTfidf);
-    console.log("this is the customerNumber:",customerTfidfData);
-console.log("All customer messages added to TF-IDF");
+  const intentsData = JSON.parse(
+    fs.readFileSync("../data/processed/final-intents.json", "utf8")
+  );
+
+  const descriptions = intentsData.intents.map(
+    (intent) => intent.description
+  );
+  descriptions.forEach((description) => {
+   intentTfidf.addDocument(description);
   });
-  console.log("Number of intent descriptions:", descriptions.length);
+
+  fs.createReadStream("../data/processed/amazonhelp.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+     customerTfidf.addDocument(row.customer_message);
+    })
+    .on("end", () => {
+      console.log("All customer messages added to TF-IDF");
+    });
+    
+
+
+    
 
 
 
