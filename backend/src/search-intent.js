fda1ea40246d8f60c978349a19c7e14c6e29cc2f
Sudeep <sudeepsudee828@gmail.com>
2026-09-26 11:55:00 +0530

    feat: search similar conversations in Qdrant

--- backend/src/search-intent.js
+++ backend/src/search-intent.js
@@ -22,6 +22,7 @@ const customerEmbedding = response.data[0].embedding;
 const result = await qdrant.search("amazonhelp", {
   vector: customerEmbedding,
   limit: 1,
+  
 });
 const matchedIntent = result[0].payload.intent;
 return  matchedIntent
